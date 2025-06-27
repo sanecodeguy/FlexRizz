1e8f0c43bba78cec7b52d15cbff7124799724d7a
sanecodeguy <rizvicottage2@gmail.com>
2025-06-27 23:44:37 +0500

    Updated URL Fetch

--- background.js
+++ background.js
@@ -7,13 +7,13 @@ extAPI.runtime.onInstalled.addListener(() => {
 
 // Firefox-compatible browser action handler
 extAPI.browserAction?.onClicked.addListener((tab) => {
-  if (tab.url.includes('flexstudent.nu.edu.pk/')) {
+  if (tab.url.includes('flexstudent.nu.edu.pk')) {
     extAPI.tabs.executeScript(tab.id, { file: 'contentLoader.js' });
   }
 });
 
 extAPI.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && tab.url.includes('flexstudent.nu.edu.pk/')) {
+  if (changeInfo.status === 'complete' && tab.url.includes('flexstudent.nu.edu.pk')) {
     extAPI.tabs.executeScript(tabId, { file: 'contentLoader.js' });
   }
 });
